33b0514ed23d7ace30345eb8fc9fab270d4d229e
aditya <adityasharma10107@gmail.com>
2022-06-23 00:25:16 +0530

    if else

---
+++ src/Rishu/RishabhDemo.java
@@ -0,0 +1,44 @@
+package Rishu;
+
+public class RishabhDemo {
+    public static void main(String[] args ){
+        System.out.println("Devloper is Online");
+        String name = "Abhi";
+        int age = 25;
+        double fee = 25000.00;
+        String name1 = "Golu";
+        int age1 = 26;
+        double fee1 = 26000.00;
+        String name2 = "rishi";
+        int age2 = 27;
+        double fee2 = 27000.00;
+        String name3  = "komal";
+        int age3 = 28;
+        double fee3 = 28000.00;
+        String name4 = "ramu";
+        int age4 = 29;
+        double fee4 = 29000.00;
+        String name5 = "raghvandra";
+        int age5 = 30;
+        double fee5 = 30000.00;
+        String name6 = "akib";
+        int age6 = 31;
+        double fee6 = 31000.00;
+        String name7 = "wrong";
+        int age7  = 32;
+        double fee7 = 32000.00;
+        String name8 = "Bittu";
+        int age8 = 33;
+        double fee8 = 33000.00;
+        System.out.println("1st Devloper name is : " + name + " age : " + age + " fee : " + fee );
+        System.out.println("2nd Devloper name is : " + name1 + " age : " + age1 + " fee : " + fee1 );
+        System.out.println("3rd Devloper name is : " + name2 + " age : " + age2 + " fee : " + fee2 );
+        System.out.println("4th Devloper name is : " + name3 + " age : " + age3 + " fee : " + fee3 );
+        System.out.println("5th Devloper name is : " + name4 + " age : " + age4 + " fee : " + fee4 );
+        System.out.println("6th Devloper name is : " + name5 + " age : " + age5 + " fee : " + fee5 );
+        System.out.println("7th Devloper name is : " + name6 + " age : " + age6 + " fee : " + fee6 );
+        System.out.println("8th Devloper name is : " + name7 + " age : " + age7 + " fee : " + fee7 );
+        System.out.println("9th Devloper name is : " + name8 + " age : " + age8 + " fee : " + fee8 );
+
+    }
+}
---
+++ src/Rishu/opdata.java
@@ -0,0 +1,52 @@
+package Rishu;
+
+public class opdata {
+    public static void main( String[] args){
+        System.out.println("hey this is my practice class");
+        String name = "Aditya";
+        int age = 21;
+        double salary = 25000.00;
+        String name1 = "Sonu";
+        int age1 = 23;
+        double salary1 = 26000.00;
+        String name2 = "Devansh";
+        int age2 = 5;
+        double salary2 = 10000.00;
+        String name3 = "Rishi";
+        int age3 = 28;
+        double salary3 = 25000.00;
+        String name4 = "Rishabh";
+        int age4 = 25;
+        double salary4 = 18000.00;
+        String name5 = "Rinku ";
+        int age5 = 32;
+        double salary5 = 28000.00;
+        String name6 = "Dinesh";
+        int age6 = 52;
+        double salary6 = 10000.00;
+        String name7 = "Asha sharma";
+        int age7 = 62;
+        double salary7 = 22500.00;
+        String name8 = "geeta";
+        int age8 = 30;
+        double salary8 = 10000.00;
+        String name9 = "Rini";
+        int age9 = 20;
+        double salary9 = 10000.00;
+        System.out.println("Name : " + name + " : Age : " + age + " : Salary : " + salary );
+        System.out.println("Name : " + name1 + " : Age : " + age1 + " : Salary : " + salary1 );
+        System.out.println("Name : " + name2 + " : Age : " + age2 + " : Salary : " + salary2 );
+        System.out.println("Name : " + name3 + " : Age : " + age3 + " : Salary : " +salary3 );
+        System.out.println("Name : " + name4 + " : Age : " + age4 + " : Salary : " +salary4 );
+        System.out.println("Name : " + name5 + " : Age : " + age5 + " : Salary : " +salary5 );
+        System.out.println("Name : " + name6 + " : Age : " + age6 + " : Salary : " + salary6 );
+        System.out.println("Name : " + name7 + " : Age : " + age7 + " : Salary : " + salary7 );
+        System.out.println("Name : " + name8 + " : Age : " + age8 + " : Salary : " + salary8 );
+        System.out.println("Name : " + name9 + " : Age : " + age9 + " : Salary : " + salary9 );
+
+
+    }
+
+
+}
+
